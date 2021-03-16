ff126dc3eae6bf0df4126ba7d749a4a04fb59f33
garciajordy <jordygarcia0119@gmail.com>
2021-03-16 12:39:50 +0200

    Update Gemfile

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,11 +1,11 @@
 def bubble_sort(array)
   return array if array.length <= 1
 
-  sum = array.length - 1
+  sum1 = array.length - 1
   loop do
-    break if sum <= 0
+    break if sum1 <= 0
 
-    sum -= 1
+    sum1 -= 1
     array.each do |n|
       x = array.index(n)
       next unless array[x + 1]
