a65eabb40e640a7ce9f96b690c6f5d2f9b069514
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-03-16 10:27:31 +0100

    Add bubble_sort method

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,3 +1,25 @@
-def bubblesort(arr)
-  arr
+def bubble_sort(array) 
+  return array if array.length <= 1
+  sum = array.length - 1
+  loop do
+   if sum <= 0
+    break
+   end
+    sum -= 1
+    puts sum 
+    array.each {|n|
+  x = array.index(n)
+  if array[x+1] 
+  
+  if n > array[x+1] 
+    array[x], array[x+1] = array[x+1], array[x]
+    p array
+  end
+
 end
+  } 
+
+    
+  end
+ end
+ p bubble_sort([4,3,78,2,0,2])
